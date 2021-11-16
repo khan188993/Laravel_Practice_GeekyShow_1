248f23391155739632723110032996511d833dfd
arfan <khan188993@gmail.com>
2021-11-16 15:10:06 +0600

    1)fresh coding

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,4 @@ use App\Http\Controllers\studentController;
 use App\Http\Controllers\ShowController;
 use GuzzleHttp\Middleware;
 
-Route::get('pagination',[studentController::class,'showDB']);
-route::get('cs',[studentController::class,'showDB']);
+
